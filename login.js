a401d705968dc6477688f0d76c0ba8b0fe8d2b32
defalt_ <sauravyad@cse.iitb.ac.in>
2018-09-14 17:16:55 +0530

    focus added to input field on load

--- login.js
+++ login.js
@@ -1,3 +1,7 @@
-$(function() {
+$(function () {
+	// for animation in in side-bar
 	particlesJS.load('init_info', 'assets/particles.json');
+
+	// to set focus initially on ldap
+	$('#ldap').focus();
 });
